5d546b8de708a2b6456a3a3fa8c82302a7681c9a
ilove-datascience <151060515+ilove-datascience@users.noreply.github.com>
2024-04-20 21:29:58 +0800

    Update Question5.java

--- Question5.java
+++ Question5.java
@@ -1,31 +1,29 @@
-import java.util.Scanner;
+import java.util.*;
 
-public class Question5
-{
-  public static void main(String[] args)
-  {
-    /**
-     * Prompt the user for number. This input indicates the number of integers the user will be entering next. 
-     * Print out the mode (highest occurrence) from the set of integers. 
-     *    e.g.
-     *     > 5
-     *     > 2
-     *     > 4
-     *     > 1
-     *     > 3
-     *     > 4
-     *     4
-     * 
-     *    e.g.
-     *     > 4
-     *     > 2
-     *     > 2
-     *     > 3
-     *     > 3
-     *     2
-     * Hint: Use a loop to get input. Use another 2 loops to find the mode
-     */
-         Scanner in = new Scanner(System.in);
+public class Question5 {
+    public static void main(String[] args) {
+        /**
+         * Prompt the user for number. This input indicates the number of integers the user will be entering next. 
+         * Print out the mode (highest occurrence) from the set of integers. 
+         *    e.g.
+         *     > 5
+         *     > 2
+         *     > 4
+         *     > 1
+         *     > 3
+         *     > 4
+         *     4
+         * 
+         *    e.g.
+         *     > 4
+         *     > 2
+         *     > 2
+         *     > 3
+         *     > 3
+         *     2
+         * Hint: Use a loop to get input. Use another 2 loops to find the mode
+         */
+        Scanner in = new Scanner(System.in);
         int times = in.nextInt();
         List<Integer> list = new ArrayList<Integer>();
         Map<Integer, Integer> map = new HashMap<Integer, Integer>();
@@ -52,4 +50,6 @@ public class Question5
         }
 
         System.out.println(mode);
+    }
 }
+

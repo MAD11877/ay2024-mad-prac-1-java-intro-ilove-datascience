3cd40ee097d753c12ce0198c60ee127ee1515fb4
ilove-datascience <151060515+ilove-datascience@users.noreply.github.com>
2024-04-20 21:58:30 +0800

    Update Question4.java

--- Question4.java
+++ Question4.java
@@ -5,22 +5,8 @@ public class Question4
 {
   public static void main(String[] args)
   {
-    /**
-     * Prints a right angle triangle with *. The base of the triangle depends on the integer given.
-     *    e.g.
-     *     > 5
-     *     *****
-     *     ****
-     *     ***
-     *     **
-     *     *
-     * 
-     * Hint: 2 loops are required. System.out.print(...) to print on single line.
-     */
-     
     Scanner in = new Scanner(System.in);
     int num = in.nextInt();
-    int count =0;
     while (num >= 1)
       {
         for (int i =0;i<num;i++)
@@ -28,6 +14,7 @@ public class Question4
             System.out.print("*");
           }
         System.out.println("");
+        num-=1;
       }
     
   }
